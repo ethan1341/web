8aca29f780adcf3d58f6a8ee0fa8bbc5d25ceba0
ethan1341 <ethan1341@yahoo.com>
2025-02-08 23:26:05 -0500

    feature:added chakra custom theme and colors, updated component

---
+++ src/assets/logo.tsx
@@ -0,0 +1,16 @@
+import {Text} from "@chakra-ui/react";
+
+const EPLogo = () => {
+    return (
+        <>
+            <style>
+                {`@import url('https://fonts.googleapis.com/css2?family=Swanky+and+Moo+Moo&display=swap');`}
+            </style>
+            <Text x="10" y="70" fontFamily="'Swanky and Moo Moo', cursive" fontSize="4xl" fill="black">
+                EP
+            </Text>
+       </>
+    );
+};
+
+export default EPLogo;
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { ChakraProvider} from "@chakra-ui/react";
+import theme from "./theme/theme.ts";
+import App from "./App.tsx";
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+      <ChakraProvider theme={theme}>
+    <App />
+      </ChakraProvider>
+  </StrictMode>,
+)
+
+export default App;
